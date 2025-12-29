8635f974d8911dab033de145a9d059ca1c899fc4
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2025-12-30 00:54:13 +0100

    Initial commit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS-Public/releases/download/1.0.0/SparteoSDK.xcframework.zip",
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/1.0.0/SparteoSDK.xcframework.zip",
             checksum: "654dcb25e4d35ca65c65dccec49abbdf679bf64247cf905e5b2a48657b8e9aba"
         )
     ]
