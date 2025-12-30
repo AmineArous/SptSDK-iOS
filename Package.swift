4a78ff85c3420457c7dfe1b330f12ec49df177b7
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2025-12-30 03:04:11 +0100

    Initial commit

--- Package.swift
+++ Package.swift
@@ -9,14 +9,27 @@ let package = Package(
     products: [
         .library(
             name: "SparteoSDK",
-            targets: ["SparteoSDK"]
+            targets: ["SparteoSDKTarget"]
         )
     ],
     targets: [
+        .target(
+            name: "SparteoSDKTarget",
+            dependencies: [
+                .target(name: "SparteoSDK"),
+                .target(name: "PrebidMobile")
+            ],
+            path: "SparteoSDKTarget"
+        ),
         .binaryTarget(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/1.0.8/SparteoSDK.xcframework.zip",
-            checksum: "9d0bd3b2a33ebae2a4ddba81ccf2122825a8e270073bb2b90a0490ecce52ce3f"
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/1.0.9/SparteoSDK.xcframework.zip",
+            checksum: "cc96e2d8fdc52a010daf63232f4fd0f89927959560bbc778f05b395f91a940dc"
+        ),
+        .binaryTarget(
+            name: "PrebidMobile",
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/1.0.9/PrebidMobile.xcframework.zip",
+            checksum: "e6cab9b41ac92d60df1a88941a491afb17532f46adea7e46464e056361a5f2f8"
         )
     ]
 )
