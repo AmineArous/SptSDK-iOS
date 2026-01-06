1ebef342d0cf53b2909860538f235a4b1b90ee9f
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2026-01-06 18:16:32 +0100

    Release 3.0.5 - Merged SparteoSDK + PrebidMobile (OMSDK already included)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/3.0.4/SparteoSDK.xcframework.zip",
-            checksum: "9d1f1b789cdcb03d2f930c01d7aab5c9e1cead3b434d9ba3220880768466b5f8"
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/3.0.5/SparteoSDK.xcframework.zip",
+            checksum: "dd9432403b7cfc0cf3951cfc28aff3f21d6e270ae23d9e00d873091560d54220"
         )
     ]
 )
