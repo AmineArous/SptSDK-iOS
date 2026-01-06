d92a7a7c3b8dc94291cad1088768ede246e6aed7
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2026-01-06 11:48:38 +0100

    Release 2.0.0 - Merged SDK with simplified API
    Breaking changes:
    - New API: Sparteo.enrichBanner(request:config:completion:)
    - New API: Sparteo.enrichInterstitial(request:config:completion:)
    - PrebidMobile now merged into SparteoSDK (single binary)
    - Removed SparteoAdView wrapper approach
    
    New features:
    - SparteoBannerConfig with SparteoBannerSize enum
    - SparteoInterstitialConfig
    - Version read from bundle Info.plist

--- Package.swift
+++ Package.swift
@@ -9,19 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "SparteoSDK",
-            targets: ["SparteoSDK", "PrebidMobile"]
+            targets: ["SparteoSDK"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/1.0.10/SparteoSDK.xcframework.zip",
-            checksum: "cc96e2d8fdc52a010daf63232f4fd0f89927959560bbc778f05b395f91a940dc"
-        ),
-        .binaryTarget(
-            name: "PrebidMobile",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/1.0.10/PrebidMobile.xcframework.zip",
-            checksum: "9a1e20932396d20bace2e6344fc01047b9bb7658f640f91a3ddb863e82e6b3cb"
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/2.0.0/SparteoSDK.xcframework.zip",
+            checksum: "c53c91448259475714962e57f8e93b2498a644abed9beeb674b1670e5f68894e"
         )
     ]
 )
