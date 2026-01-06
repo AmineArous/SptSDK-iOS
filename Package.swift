906d0c82478826729d1c34a767d4770d0e13f725
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2026-01-06 14:52:41 +0100

    Light version with PrebidMobile as dependency - version 2.0.6

--- Package.swift
+++ Package.swift
@@ -12,11 +12,21 @@ let package = Package(
             targets: ["SparteoSDK"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/prebid/prebid-mobile-ios.git", from: "2.2.0")
+    ],
     targets: [
         .binaryTarget(
+            name: "SparteoSDKBinary",
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/2.0.6/SparteoSDK.xcframework.zip",
+            checksum: "c427006f84f989da47580464bd8ad7989bdcd8f1d9d86a4e8d85b546973caaa5"
+        ),
+        .target(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/2.0.5/SparteoSDK.xcframework.zip",
-            checksum: "12005af4c3ee6117a369cdbb6123a9ca5a278e55878d94aebb9222aadd37022a"
+            dependencies: [
+                "SparteoSDKBinary",
+                .product(name: "PrebidMobile", package: "prebid-mobile-ios")
+            ]
         )
     ]
 )
