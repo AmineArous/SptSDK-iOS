b5f17a9b5eaa3e3d0d8e4fd99bc632c92d7e79ea
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2026-01-06 16:51:41 +0100

    v3.0.0: Merged SparteoSDK + PrebidMobile into single framework
    - PrebidMobile is now embedded in SparteoSDK
    - Single dependency instead of 3
    - Added initialization completion callback

--- Package.swift
+++ Package.swift
@@ -9,24 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "SparteoSDK",
-            targets: ["SparteoSDK", "PrebidMobile", "OMSDK_Prebidorg"]
+            targets: ["SparteoSDK"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/2.2.0/SparteoSDK.xcframework.zip",
-            checksum: "6c54ca5d29490767a634cc2f5ba63802a43cb3f7a2cf59e55ca9ec7ddc1d925c"
-        ),
-        .binaryTarget(
-            name: "PrebidMobile",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/2.2.0/PrebidMobile.xcframework.zip",
-            checksum: "c32692993b173d42c932d2bde80bb52c903133fc2caf23036a0fda903dda6deb"
-        ),
-        .binaryTarget(
-            name: "OMSDK_Prebidorg",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/2.2.0/OMSDK_Prebidorg.xcframework.zip",
-            checksum: "ed850cd5ca4bb583f51a110c7f03d083127aa178d9a89fc0ac76ebbe2075c5f1"
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/3.0.0/SparteoSDK.xcframework.zip",
+            checksum: "b4f3aa42e807de1b58fb844c63fadee472625b5cbf5376ac9a82d8271c48f378"
         )
     ]
 )
