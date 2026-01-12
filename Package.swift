0579f417bd0cdd7488aa7eac6757a806f6312bdd
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2026-01-12 21:55:15 +0100

    Release 3.1.2 - Fix interstitial video parameters

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SparteoSDK",
-            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/3.1.1/SparteoSDK.xcframework.zip",
+            url: "https://github.com/AmineArous/SptSDK-iOS/releases/download/3.1.2/SparteoSDK.xcframework.zip",
             checksum: "d1eb4c6440feafbc3bf2163b6f3af549a46de1222bbf7d0d92df63acf0a7dbf4"
         )
     ]
